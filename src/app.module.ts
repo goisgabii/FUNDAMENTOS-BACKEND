5b169a521f1e7e722365dd6d3df1c5b9fd8f6084
Juliano Ramos <juliano.ramos@toglab.co>
2025-06-10 19:20:03 -0300

    CRUD

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CreateProductController } from './create-product.controller';
+import { PrismaService } from './prisma.service';
+import { CreateProductService } from './create-product.service';
+import { ProductsRepository } from './products.repository';
+
+@Module({
+  imports: [],
+  controllers: [CreateProductController],
+  providers: [PrismaService, CreateProductService, ProductsRepository],
+})
+export class AppModule {}
